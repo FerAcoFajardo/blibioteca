85d72505eae129396ef5e039ecffae3347f32caf
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-01-16 15:17:01 -0700

    Primer commit

---
+++ app/Controllers/libros/ListarLibro.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class CrearLibro extends Controller{
+    public function form(){
+        return view('libros/listar');
+    }
+
+}
